62258e8ac1e6ac26c12fd403b28d6c16e8457405
JP Whitaker <jpw@hawaii.edu>
2023-08-25 17:28:00 -1000

    transform perspective working

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -1,28 +1,23 @@
-
-import { useState, useEffect, useRef, MouseEvent, MouseEventHandler } from 'react';
+import { useState, useEffect, useRef, MouseEvent } from 'react';
 import { motion, useMotionValue, Transition } from "framer-motion";
-import styles from './HoverCard.module.css';
-
-
 
 const HoverCard = ({ i }: { i: number }) => {
   const [isMaximized, setisMaximized] = useState<boolean>(false);
   const [isHovered, setHovered] = useState(false)
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
+  const [rx, setRx] = useState(0);
+  const [ry, setRy] = useState(0);
   const cardRef = useRef<HTMLDivElement>(null)
   const transform = useMotionValue('rotateX(0deg)');
+
   const angle = 40;
 
-
-  const message = ['ho', 'brah', 'sup', "ka'a", 'and', 'angelo', '!', ''];
+  const message = ['1', '2', '3', "4", '5', '6', '!', ''];
 
   const handleClick = () => {
     setisMaximized(!isMaximized);
   };
-
-
-
 
   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
     // Get mouse coordinates relative to the page
@@ -45,34 +40,33 @@ const HoverCard = ({ i }: { i: number }) => {
     };
   };
 
-  
+  const handleMouseLeave = () => {
+    setRx(0);
+    setRy(0);
+    // setIsHovering(false);
+  };
 
 
   //rotation
   useEffect(() => {
-    // console.log(`x: ${mouseX} y: ${mouseY}`);
-    const theta = Math.atan2(mouseX, mouseY)
-
     if (cardRef.current) {
       const rect = cardRef.current.getBoundingClientRect();
       const width = rect.width
       const height = rect.height
       const degX = width/angle
       const degY = height / angle
-      const rx = -(mouseY / degY)
-      const ry = (mouseX / degX)
-      var newTransform = 'perspective(1000px) rotateX(' + rx + 'deg) rotateY(' + ry + 'deg)';
-      transform.set(newTransform);
+      setRx( -(mouseY / degY))
+      setRy(mouseX / degX)
+      // var newTransform = 'perspective(1000px) rotateX(' + rx + 'deg) rotateY(' + ry + 'deg)';
+      // transform.set(newTransform);
     }
-
-    
   }, [mouseX, mouseY]);
 
 
   return (
     <>
       {/** Outer container div, constricts inner child motion div **/}
-      <div ref={cardRef} className={`rounded-[8px] aspect-[16/9]`}>
+      <div ref={cardRef} className={`rounded-[8px] aspect-[16/9]`} style={{ transform: "perspective(1000px)"}}>
         
         {/** Main motion div, grows and shrinks on hover, on click grows to full screen **/}
         <motion.div
@@ -80,9 +74,16 @@ const HoverCard = ({ i }: { i: number }) => {
           layout={true}
           transition={trans}
           // whileHover={}
-          style={{transform}}
+          onMouseLeave={handleMouseLeave}
+          initial={{ 
+            rotateX: 0,
+            rotateY: 0,
+            transformPerspective: 1000
+           }} 
           animate={{
-            
+            rotateX: rx,
+            rotateY: ry,
+            transformPerspective: 1000
           }}
           className={`bg-black  h-full p-4`}
           onClick={() => handleClick()}
@@ -100,7 +101,7 @@ const HoverCard = ({ i }: { i: number }) => {
 }
 
 const trans: Transition = {
-  duration: 0.8
+  duration: 0.2
 }
 
 export default HoverCard
