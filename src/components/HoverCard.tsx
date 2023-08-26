a708d10f6b2ef191e23f3f3dcbb24051e92ed4df
JP Whitaker <jpwhitaker@users.noreply.github.com>
2023-08-26 22:38:20 +0000

    Update HoverCard.module.css and HoverCard.tsx

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -1,26 +1,32 @@
-import { useState, useEffect, useRef, MouseEvent } from 'react';
+import { useState, useEffect, useRef, MouseEvent } from "react";
 import { motion, useMotionValue, Transition } from "framer-motion";
-import styles from './HoverCard.module.css';
+import styles from "./HoverCard.module.css";
 
 const HoverCard = ({ i }: { i: number }) => {
   const [isMaximized, setisMaximized] = useState<boolean>(false);
-  
+
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
   const [rx, setRx] = useState(0);
   const [ry, setRy] = useState(0);
-  const cardRef = useRef<HTMLDivElement>(null)
-  
+  const cardRef = useRef<HTMLDivElement>(null);
 
-  const angle = 40;
+  const angle = 80;
 
-  const message = ['bg-red-400', 'bg-blue-400', 'bg-green-400', "bg-purple-400", 'bg-orange-400', 'bg-pink-400', 'bg-yellow-400', ''];
+  const message = [
+    "bg-red-400",
+    "bg-blue-400",
+    "bg-green-400",
+    "bg-purple-400",
+    "bg-orange-400",
+    "bg-pink-400",
+    "bg-yellow-400",
+    "",
+  ];
 
   const handleClick = () => {
     setisMaximized(!isMaximized);
   };
-
-
 
   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
     // Get mouse coordinates relative to the page
@@ -29,7 +35,6 @@ const HoverCard = ({ i }: { i: number }) => {
 
     // Get the element's position and dimensions
     if (cardRef.current) {
-
       const rect = cardRef.current.getBoundingClientRect();
       const cardCenterX = rect.left + rect.width / 2;
       const cardCenterY = rect.top + rect.height / 2;
@@ -40,7 +45,7 @@ const HoverCard = ({ i }: { i: number }) => {
       // Update state
       setMouseX(relativeX);
       setMouseY(relativeY);
-    };
+    }
   };
 
   const handleMouseLeave = () => {
@@ -49,17 +54,16 @@ const HoverCard = ({ i }: { i: number }) => {
     // setIsHovering(false);
   };
 
-
   //rotation
   useEffect(() => {
     if (cardRef.current) {
       const rect = cardRef.current.getBoundingClientRect();
-      const width = rect.width
-      const height = rect.height
-      const degX = width / angle
-      const degY = height / angle
-      setRx(-(mouseY / degY))
-      setRy(mouseX / degX)
+      const width = rect.width;
+      const height = rect.height;
+      const degX = width / angle;
+      const degY = height / angle;
+      setRx(-(mouseY / degY));
+      setRy(mouseX / degX);
       // var newTransform = 'perspective(1000px) rotateX(' + rx + 'deg) rotateY(' + ry + 'deg)';
       // transform.set(newTransform);
     }
@@ -68,8 +72,7 @@ const HoverCard = ({ i }: { i: number }) => {
   return (
     <>
       {/** Outer container div, constricts inner child motion div **/}
-      <div ref={cardRef} className={`rounded-[8px] aspect-[16/9]`} >
-
+      <div ref={cardRef} className={`rounded-[8px] aspect-[16/9]`}>
         {/** Main motion div, grows and shrinks on hover, on click grows to full screen **/}
         <motion.div
           onMouseMove={handleMouseMove}
@@ -79,30 +82,34 @@ const HoverCard = ({ i }: { i: number }) => {
           initial={{
             rotateX: 0,
             rotateY: 0,
-            transformPerspective: 1000
+            transformPerspective: 1000,
           }}
           animate={{
             rotateX: isMaximized ? 0 : rx,
             rotateY: isMaximized ? 0 : ry,
-            transformPerspective: 1000
+            transformPerspective: 1000,
           }}
-          className={`rounded-[8px] ${message[i]}   h-full p-4 ${isMaximized ? 'maximized' : ''}`}
+          className={`rounded-[8px] ${message[i]} cursor-pointer h-full p-4 ${
+            isMaximized ? "maximized" : ""
+          }`}
           onClick={() => handleClick()}
         >
           {/** Inner Content, shifts position with same transition speed **/}
           <motion.div
             transition={trans}
             layout="position"
-            className="text-white">
-            {message[i]}</motion.div>
+            className="text-white"
+          >
+            {message[i]}
+          </motion.div>
         </motion.div>
       </div>
     </>
-  )
-}
+  );
+};
 
 const trans: Transition = {
-  duration: 0.2
-}
+  duration: 0.2,
+};
 
-export default HoverCard
+export default HoverCard;
